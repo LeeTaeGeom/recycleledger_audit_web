c174e113d800cb13eb6845a0e277fa527595a744
이태검 <taegeom0601@gmail.com>
2022-05-15 18:41:14 +0900

    db 저장 및 postlist

--- post/models.py
+++ post/models.py
@@ -7,13 +7,12 @@ class Document(models.Model):
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
 
 # views.py에 작성했던 Report 모델 예시
-"""
+
 class Report(models.Model):
-    date = models.CharField(max_length=200)
-    PO_name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200) 
-    collector = models.CharField(max_length=200) 
-    quantity = models.CharField(max_length=200) 
-    converted_qty = models.CharField(max_length=200) 
-    conllecting_company = models.CharField(max_length=200)
-"""
+    date = models.CharField(max_length=200,null=True)
+    PO_name = models.CharField(max_length=200,null=True)
+    address = models.CharField(max_length=200,null=True) 
+    collector = models.CharField(max_length=200,null=True) 
+    quantity = models.IntegerField(null=True) 
+    converted_qty = models.FloatField(null=True) 
+    conllecting_company = models.CharField(max_length=200,null=True)
